ff0591da016c4db40f2fe2bc9a4ddcd0849fa663
Will Downey <willd20@vt.edu>
2022-07-16 16:15:35 -0400

    Complete exercise 3.3

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.get('/api/persons', (req, res) => {
 app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date().toString()}</p>`);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if(person) res.json(person);
+    else res.status(404).end();
 })
 
 const PORT = 3001;
