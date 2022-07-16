43163df79611a78948487989d9ba5bd183956714
Will Downey <willd20@vt.edu>
2022-07-16 16:11:28 -0400

    Complete exercise 3.2

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date().toString()}</p>`);
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
